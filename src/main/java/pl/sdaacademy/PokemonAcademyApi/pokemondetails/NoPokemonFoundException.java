b8d927cc7c4a69bd1e7efcb77abea1ca6b3f5ac9
Angela <user@Angelas-Macbook-Pro.local>
2021-09-26 12:17:39 +0100

    feature/PAA-5 : implement feature

---
+++ src/main/java/pl/sdaacademy/PokemonAcademyApi/pokemondetails/NoPokemonFoundException.java
@@ -0,0 +1,9 @@
+package pl.sdaacademy.PokemonAcademyApi.pokemondetails;
+
+public class NoPokemonFoundException extends RuntimeException {
+
+    public NoPokemonFoundException(String pokemonName){
+        super(String.format("No pokemon with %s name found!", pokemonName));
+    }
+
+}
