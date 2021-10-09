70c2b2bb503fac709b8b36461bf61d8c363a18be
Angela <user@Angelas-Macbook-Pro.local>
2021-10-09 13:54:41 +0100

    zabezpieczenia

---
+++ src/main/java/pl/sdaacademy/PokemonAcademyApi/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package pl.sdaacademy.PokemonAcademyApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.sdaacademy.PokemonAcademyApi.security.AuthenticationFilter;
+import pl.sdaacademy.PokemonAcademyApi.security.AuthorizationFilter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final String headerKey;
+    private final String signature;
+    private final String tokenType;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfiguration(@Value("${paa.authorization-key}") String headerKey,
+                                 @Value("${paa.signature}") String signature,
+                                 @Value("${paa.token-type}") String tokenType,
+                                 UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+        this.headerKey = headerKey;
+        this.signature = signature;
+        this.tokenType = tokenType;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/pokemon/**").authenticated()
+                .antMatchers("/user").permitAll()
+                .and()
+                .csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new AuthenticationFilter(headerKey, signature, tokenType, authenticationManager()))
+                .addFilter(new AuthorizationFilter(headerKey, signature, tokenType, authenticationManager()))
+                .headers().frameOptions().disable();//obłsuga h2
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
+}
---
+++ src/main/java/pl/sdaacademy/PokemonAcademyApi/user/UserController.java
@@ -0,0 +1,20 @@
+package pl.sdaacademy.PokemonAcademyApi.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public User singUp(@RequestBody User user){
+        return userService.addUser(user);
+    }
+
+}
